7a811e959f8618ef536ce82560507914fefdabee
countingpigeons <49214807+countingpigeons@users.noreply.github.com>
2019-09-25 18:03:49 -0700

    local changes to Main - added pycharm integration

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import random
 import pandas as pd
 
-values = ['2','3','4','5','6','7','8','9','10','j','q','k','a']
-suits = ['_h','_c','_s','_d']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']
+suits = ['_h', '_c', '_s', '_d']
 
-print(values)
-print(suits)
+# print(values)
+# print(suits)
 
 cards = []
 for suit in suits:
@@ -16,18 +16,25 @@ for suit in suits:
 
 # print(cards)
 decks = []
-random.shuffle(cards)
 
-decks.append(cards)
-# print(decks)
+#
+# random_range = np.arange(0,1,.01)
+# print(random_range)
 
-df = pd.DataFrame(decks)
+# for _ in np.arange(0, 1, .01):
+#     random.shuffle(cards, random=_)
+#     decks.append(cards)
+#     # print(decks)
+# #
+# #
+# df = pd.DataFrame(decks)
+# print(df.head())
 
-df.head()
-
-
+# print(help(random))
 
 
 # print(deck)
-
 # print(help(random))
+
+for x in np.arange(0,1,.01):
+    print(x)
