d788556341d988d35b217fc5d23ab98b0bdc3249
countingpigeons <49214807+countingpigeons@users.noreply.github.com>
2019-10-09 20:26:08 -0700

    Updated main script with new name 'winning_deck'.

---
+++ winning_deck.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+
+
+def create_random_deck_indexes(n=5):
+    np.random.seed(0)
+    index = np.arange(0, 52)
+    indexes = []
+    for _ in np.arange(n):
+        indexes.append(np.random.permutation(index))
+    return indexes
+
+
+class Base:
+    suits = ['S', 'C', 'H', 'D']
+    cards = list(np.arange(2, 11).astype('str'))
+    cards.extend('JQKA')
+
+    base_deck_list = []
+    for suit in suits:
+        for card in cards:
+            base_deck_list.append(card+'_'+suit)
+
+    cardvalues = cards * 4
+    colors = ['Red']*26
+    colors.extend(['Black']*26)
+
+    deck = pd.Series(base_deck_list)
+
+
+class Deck:
+    def __init__(self, index):
+        self.deck = Base.deck[index].values
+        self.cardvalues = Base.cardvalues
+        self.colors = Base.colors
+        pass
+
+    def summary(self):
+        print('OriginalDeckOrder: {}'.format(self.deck))
+        print('CardValues: {}'.format(self.cardvalues))
+        print('Colors: {}'.format(self.colors))
+
+    def deal_piles(self):
+        pass
+
+    def move_suits_up(self):
+        pass
+
+    def move_suits_down(self):
+        pass
+
+    def move_cards_between_piles(self):
+        pass
+
+    def deal_to_play_pile(self):
+        pass
+
+    def play_onto_piles(self):
+        pass
+
+#     HOW TO STORE CARD COLORS? IN A DICTIONARY AGAINST THE UNSORTED BASE.DECK PHAPS?
+#     ALSO, I WILL WANT SOME KIND OF LOGGING AS WE 'PLAY' SO I CAN QA/SPOT-CHECK THE LOGIC (PHAPS W/ REAL DECK?)
+
+def play(deck):
+    # deck[]
+    pass
+
+def main():
+    indexes = create_random_deck_indexes()
+    deck = Deck(indexes[0])
+
+    deck.summary()
+    # print(deck.deck)
+    # print(Base.deck.values)
+    # print('ran main()')
+
+
+main()
