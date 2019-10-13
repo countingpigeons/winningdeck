223b42252a6abce8e2076cc35e49717da076664b
countingpigeons <49214807+countingpigeons@users.noreply.github.com>
2019-10-13 14:53:34 -0700

    added deal 7 piles

--- winning_deck.py
+++ winning_deck.py
@@ -48,19 +48,95 @@ class SolitaireGame:
             deck.append(item + [randindex[i]])
             i += 1
         deck = sorted(deck, key=lambda x: x[4])
-        # deck = [tuple(item) for item in deck]
+        # deck = [tuple(item) for item in deck] # tuple safer, but inconvenient
         deck = list(
             map(lambda x: {'label': x[0], 'value': x[1],
                            'color': x[2], 'suit': x[3]}, deck))
         self.deck = deck
+        self.spade_pile = []
+        self.club_pile = []
+        self.heart_pile = []
+        self.diamond_pile = []
+        self.pile1 = []
+        self.pile2 = []
+        self.pile2_hidden = []
+        self.pile3 = []
+        self.pile3_hidden = []
+        self.pile4 = []
+        self.pile4_hidden = []
+        self.pile5 = []
+        self.pile5_hidden = []
+        self.pile6 = []
+        self.pile6_hidden = []
+        self.pile7 = []
+        self.pile7_hidden = []
+        self.play_pile = []
+        # self.reserve_pile = []
 
     def summary(self):
+
+        piles = [self.pile1, self.pile2, self.pile3,
+                 self.pile4, self.pile5,
+                 self.pile6, self.pile7]
+
+        for pile in piles:
+            print((pile[0]['label']))
+
         # print('OriginalDeckOrder: {}\n'.format(Base.deck))
-        print('ShuffledDeck: {}\n'.format(list(map(lambda x: x['label'],
-                                                   self.deck))))
+        # print('ShuffledDeck: {}\n'.format(list(map(lambda x: x['label'],
+        #                                            self.deck))))
+        # for card in self.deck:
+        #     print(card['label'])
+
+        print('_____ran summary_____')
+        pass
 
     def deal_piles(self):
-        pass
+
+        if len(self.deck) == 52:
+            hiddenpiles = [self.pile2_hidden, self.pile3_hidden,
+                           self.pile4_hidden, self.pile5_hidden,
+                           self.pile6_hidden, self.pile7_hidden]
+
+            piles = [self.pile1, self.pile2, self.pile3,
+                     self.pile4, self.pile5,
+                     self.pile6, self.pile7]
+
+            self.pile1.append(self.deck.pop(0))
+            for pile in hiddenpiles[:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile2.append(self.deck.pop(0))
+            for pile in hiddenpiles[1:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile3.append(self.deck.pop(0))
+            for pile in hiddenpiles[2:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile4.append(self.deck.pop(0))
+            for pile in hiddenpiles[3:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile5.append(self.deck.pop(0))
+            for pile in hiddenpiles[4:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile6.append(self.deck.pop(0))
+            for pile in hiddenpiles[5:]:
+                pile.append(self.deck.pop(0))
+
+            self.pile7.append(self.deck.pop(0))
+
+# test out
+            # for pile in hiddenpiles:
+            #     print((pile))
+            # for pile in piles:
+            #     print((pile))
+            # for card in self.pile7_hidden:
+            #     print(card['label'])
+            # print(self.pile7_hidden)
+            print('_____ran deal_piles_____')
 
     def move_suits_up(self):
         pass
@@ -81,16 +157,18 @@ class SolitaireGame:
 # QA/SPOT-CHECK THE LOGIC (PHAPS W/ REAL DECK?)
 
 
-def PlaySolitaire(numdecks):
+def main(numdecks):
 
     decks = create_random_deck_indexes(numdecks)
     game = SolitaireGame(decks[0])
+    game.deal_piles()
 
     # print(game.deck[:3][-1])
     # print('')
+    # print(game.deck)
     game.summary()
-    print('_____ran PlaySolitaire()_____')
+    print('_____ran main_____')
 
 
 if __name__ == "__main__":
-    PlaySolitaire(numdecks=8)
+    main(numdecks=8)
