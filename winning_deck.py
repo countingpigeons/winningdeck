aaad00fb1de33bc4ec4c07b2bc74b234e45f048c
countingpigeons <49214807+countingpigeons@users.noreply.github.com>
2020-03-08 23:22:35 -0700

    Updated output file name to winning_deck_results.

--- winning_deck.py
+++ winning_deck.py
@@ -973,7 +973,7 @@ def main(numdecks):
               format(tot_decks, won_decks, won_decks / tot_decks, num_moves,
                      num_moves/tot_decks))
 
-        with open('deck_results', 'w') as f:
+        with open('winning_deck_results', 'w') as f:
             card_labels = ''
             for index in range(51):
                 card_labels += 'x'+str(index)+','
@@ -990,7 +990,7 @@ def main(numdecks):
                                                result['won'],
                                                card_vals)
                 f.write(line + '\n')
-        msg = '''"deck_results" written to local directory with {} rows.'''
+        msg = '''"winning_deck_results" written to local dir with {} rows.'''
         print(msg.format(tot_decks))
 
     elif type == 'testing':
